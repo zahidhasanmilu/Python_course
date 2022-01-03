4cd79b41f5f0344db66da0aea6a1d3c77939da0b
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-01-04 01:12:16 +0600

    os modules

---
+++ os_module.py
@@ -0,0 +1,13 @@
+import os
+print(dir(os))
+print(os.getcwd())
+os.chdir("C://")
+print(os.getcwd())
+print(os.listdir("C://"))
+os.makedirs("This/that")
+os.rename("harry.txt", "Milu.txt")
+print(os.environ.get('Path'))
+print(os.path.join("C:/", "/Milu.txt"))
+
+print(os.path.exists("C://Program Files2"))
+print(os.path.isfile("C://Program Files"))
